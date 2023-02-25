ad11a32cfc90537373a7b7cf3427cdc97809f523
Valentino Coppola Ferrari <116271468+ValenCF@users.noreply.github.com>
2023-02-25 10:57:54 +0100

    Update ficheros_basico.c
     Para guardar el bufferMB al dispositivo virtual como tendría que hacer el bwrite?

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -26,7 +26,27 @@ int initSB(unsigned int nbloques, unsigned int ninodos) {
 }
 
 int initMB() {
+ //Calculamos los bits totales de metadatos y los pasamos a bytes
+    int tam= (tamMB()+tamAI()+tamSB)/8;
+    //realizamos el modulo del tamaño total para saber el numero de 1's
+    //del último bloque
+    int mod= tam % 8;
 
+    //declaramos el buffer
+    unsigned char bufferMB[BLOCKSIZE];
+
+    //iteramos desde i=0 a i= tam haciendo la asignación de 1's
+    for(int i =posSB;i<tam;i++){
+        buffer[i]=255;
+    }
+    //para el último byte asignamos 1's solo a los bits que corresponden
+    for (int j = 0; j<mod; j++){
+        buffer[tam]+=2^(7-j)
+    }
+    //ponemos los bytes restantes del bloque a 0
+    for(int x=tam+1;x<BLOCKSIZE;x++){
+        buffer[x]=0;
+    }
 }
 
 int initAI() {
